e0435a2bcc0c997de8c51409be90a5a8e44069a4
cboirard <cboirard@scortex.io>
2020-01-10 15:04:42 +0100

    class based component

---
+++ src/views/PageHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-container">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class PageHome extends Vue {
+}
+</script>
+
+<style class="scss" scoped>
+.page-container {
+  background-color: #eee;
+}
+</style>
